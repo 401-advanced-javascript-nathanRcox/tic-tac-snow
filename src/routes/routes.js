90713b251541817bb5cb3f9df23520b7682f07e1
Nathan Rhead Cox <nrc4@hotmail.com>
2020-12-16 21:05:30 -0800

    All routes working; api/v2 with auth and permissions working.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,10 @@
 const express = require('express');
 const authRouter = express.Router();
 
-const User = require('./models/users.js');
-const basicAuth = require('./middleware/basic.js')
-const bearerAuth = require('./middleware/bearer.js')
-const permissions = require('./middleware/acl.js')
+const User = require('../auth/models/users.js');
+const basicAuth = require('../auth/middleware/basic.js')
+const bearerAuth = require('../auth/middleware/bearer.js')
+const permissions = require('../auth/middleware/acl.js')
 
 authRouter.post('/signup', async (req, res, next) => {
   try {
@@ -31,9 +31,11 @@ authRouter.post('/signin', basicAuth, (req, res, next) => {
 });
 
 authRouter.get('/users', bearerAuth, permissions('delete'), async (req, res, next) => {
-  const users = await User.find({});
-  const list = users.map(user => user.username);
-  res.status(200).json(list);
+  try {
+    const users = await User.find({});
+    const list = users.map(user => user.username);
+    res.status(200).json(list);
+  } catch (error) { console.error(error) }
 });
 
 authRouter.get('/secret', bearerAuth, async (req, res, next) => {
--- src/server.js
+++ src/server.js
@@ -8,7 +8,10 @@ const morgan = require('morgan');
 // Esoteric Resources
 const errorHandler = require('./error-handlers/500.js');
 const notFound = require('./error-handlers/404.js');
-const authRoutes = require('./auth/routes.js');
+const logger = require('./auth/middleware/logger.js');
+const authRoutes = require('./routes/routes.js');
+const v1Routes = require('./routes/v1.js');
+const v2Routes = require('./routes/v2.js');
 
 // Prepare the express app
 const app = express();
@@ -20,18 +23,23 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(logger);
+
 // Routes
+app.use('/api/v1', v1Routes);
+app.use('/api/v2', v2Routes);
 app.use(authRoutes);
 
 // Catchalls
-app.use(notFound);
+app.use('*', notFound);
 app.use(errorHandler);
 
 module.exports = {
   server: app,
   start: (port) => {
+    if (!port) { throw new Error('The port is missing.'); }
     app.listen(port, () => {
-      console.log(`Server Up on ${port}`);
+      console.log(`Order Up on ${port}`);
     });
   },
 };
